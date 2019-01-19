f1db52d694c4f7283c26d1639d73b15a2e485af6
Rostislav Khaskin <Rostislav.Khaskin@aexp.com>
2019-01-19 16:13:17 -0500

    fixed rest

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,27 +2,47 @@ import _ from 'lodash';
 import { queryResource, queryCollection } from 'iguazu-rest';
 
 export const fetchPostsAndUsers = () => (dispatch, getState) => {
-  dispatch(fetchPosts());
-/*
+   const fetchPostsRes = fetchPosts(dispatch);
+   console.log("fetchPosts", fetchPostsRes)
+   const obj = dispatch(fetchPostsRes);
+   console.log("obj", obj);
+   
+   demo();
+   console.log("YYYYYYYY", getState());
+
   _.chain(getState().posts)
     .map('userId')
     .uniq()
     .forEach(id => dispatch(fetchUser(id)))
     .value();
-*/    
+    
+    return obj;
 };
 
 export const fetchPosts = function(dispatch) {
   return function (dispatch) {
-    dispatch(queryCollection({ resource: 'posts' }));
+    const col = queryCollection({ resource: 'posts' })
+    console.log("Col", col)
+    return dispatch(col);
   };
-  //return dispatch(queryCollection({ resource:  'posts' }));
 };
 
 export const fetchUser = id => dispatch => {
+  console.log("ID =", id);
   //const response = await jsonPlaceholder.get(`/users/${id}`);
 
   //dispatch({ type: 'FETCH_USER', payload: response.data });
   dispatch(queryResource({ resource:  'user', id }));
 };
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+  console.log('Taking a break...');
+  await sleep(2000);
+  console.log('Two seconds later');
+}
+
 
--- blog/src/actions/loadPosts.js
+++ blog/src/actions/loadPosts.js
@@ -3,13 +3,6 @@ import {fetchPostsAndUsers, fetchPosts} from './index';
 export function loadPosts(param) {
  
     return (dispatch, getState) => {
-        const data = getState().resources.posts;
-        console.log("UUU", getState().resources);
-        const status = data ? 'complete' : 'loading';
-           
-//        const promise = data && data.length > 0 ? Promise.resolve : dispatch(fetchPostsAndUsers());
-        const promise = data && data.length > 0 ? Promise.resolve : dispatch(fetchPosts());
-        
-        return {data, status, promise};
+        return dispatch(fetchPostsAndUsers());
     }
 }
